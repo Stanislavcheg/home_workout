9559a8525448f6435e1420986412ec56399adac4
Stas Makhno <Stanislav.Makhno@dataart.com>
2020-05-03 10:27:06 +0300

    Assets compressed.

--- test/controllers/gymnastic_rings/programs_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GymnasticRings::ProgramsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get gymnastic_rings_programs_index_url
-    assert_response :success
-  end
-
-end
--- test/controllers/gymnastic_rings_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GymnasticRingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get gymnastic_rings_index_url
-    assert_response :success
-  end
-
-end
